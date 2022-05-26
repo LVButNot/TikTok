75228f42b59f664dd2639d7516ef735e6fe30bb6
LVButNot <105089868+LVButNot@users.noreply.github.com>
2022-05-26 13:32:16 +0800

    更新module

--- simple-demo-main/go.mod
+++ simple-demo-main/go.mod
@@ -1,4 +1,4 @@
-module github.com/RaymondCode/simple-demo
+module github.com/LVButNot/TikTok
 
 go 1.17
 
--- simple-demo-main/main.go
+++ simple-demo-main/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
