a919e3c1a47d6243b1a903c14143524a68aec8cf
LVButNot <105089868+LVButNot@users.noreply.github.com>
2022-05-28 10:51:39 +0800

    调整评论发布接口，发布成功后直接返回评论内容用于展示，客户端不会重新刷新评论列表

--- simple-demo-main/controller/common.go
+++ simple-demo-main/controller/common.go
@@ -7,12 +7,13 @@ type Response struct {
 
 type Video struct {
 	Id            int64  `json:"id,omitempty"`
-	Author        User   `json:"author"`
+	Author        User   `json:"author" gorm:"foreignKey:Id;references:Id;"`
 	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
 	CoverUrl      string `json:"cover_url,omitempty"`
 	FavoriteCount int64  `json:"favorite_count,omitempty"`
 	CommentCount  int64  `json:"comment_count,omitempty"`
 	IsFavorite    bool   `json:"is_favorite,omitempty"`
+	Title         string `json:"title,omitempty"`
 }
 
 type Comment struct {
--- simple-demo-main/router.go
+++ simple-demo-main/router.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/RaymondCode/simple-demo/controller"
+	"github.com/LVButNot/TikTok/controller"
 	"github.com/gin-gonic/gin"
 )
 
